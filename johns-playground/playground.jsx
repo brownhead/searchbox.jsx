83088a7420b813bb7ad555e7f9a23a16a71ac173
John Sullivan <josullivan@mozilla.com>
2014-07-19 15:25:35 -0700

    Something is working.

--- johns-playground/playground.jsx
+++ johns-playground/playground.jsx
@@ -5,12 +5,24 @@ var root = {};
 root.componentStore = [];
 
 root.componentStore.SearchInput = React.createClass({
+    queryChanged: function(event) {
+        this.props.onQueryChanged(event.target.value);
+    },
+
     render: function() {
-        return <input type="text" />;
+        return <input type="text" onChange={this.queryChanged} />;
     }
 });
 
-root.componentStore.SearchResults = React.createClass({
+// root.componentStore.SearchDatasetMixin = {
+//     render: function() {
+//         return (
+//             <div>MOOOO</div>
+//         );
+//     }
+// };
+
+root.componentStore.SearchDropdown = React.createClass({
     render: function() {
         var children = [];
         for (var i = 0; i < this.props.ordering.length; ++i) {
@@ -23,6 +35,7 @@ root.componentStore.SearchResults = React.createClass({
                 curConfig.component({
                     key: curName,
                     config: curConfig,
+                    query: this.props.query,
                 })
             );
         }
@@ -44,7 +57,7 @@ root.componentStore.Search = React.createClass({
 
     getInitialState: function() {
         // The default ordering is alphabetic
-        var ordering = _.map(this.datasetConfigs, function(v, k) {
+        var ordering = _.map(this.props.datasetConfigs, function(v, k) {
             return k;
         });
         ordering.sort();
@@ -55,13 +68,17 @@ root.componentStore.Search = React.createClass({
         }
     },
 
+    onQueryChanged: function(query) {
+        this.setState({query: query});
+    },
+
     render: function() {
         return (
             <div>
                 <root.componentStore.SearchInput
-                    // onQueryChanged={this.onQueryChanged.bind(this)}
+                    onQueryChanged={this.onQueryChanged}
                     query={this.state.query} />
-                <root.componentStore.SearchResults
+                <root.componentStore.SearchDropdown
                     query={this.state.query}
                     datasetConfigs={this.props.datasetConfigs}
                     ordering={this.state.ordering} />
@@ -74,17 +91,43 @@ root.componentStore.Search = React.createClass({
 // USER LAND //
 ///////////////
 
-// var BlaItem = React.createClassWithMixin(MagicItem, {
-//     root_onSelect: function() {
-//         this.props.controller.components["footer"].updateMaybe();
-//     }
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substrRegex;
 
-//     render: function() {
-//         return <a href="#">adsf</a>
-//     }
-// });
+    // an array that will be populated with substring matches
+    matches = [];
 
-var Bla = React.createClass({
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        // the typeahead jQuery plugin expects suggestions to a
+        // JavaScript object, refer to typeahead docs for more info
+        matches.push({ value: str });
+      }
+    });
+
+    cb(matches);
+  };
+};
+
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
+var hound = substringMatcher(states);
+
+var StatesDataset = React.createClass({
     // onPropsChange: function(event) {
     //     console.log("searching for " + this.props.controller.query);
 
@@ -95,25 +138,41 @@ var Bla = React.createClass({
     //     // });
     // },
 
+    componentWillReceiveProps: function(nextProps) {
+        console.log(nextProps);
+        if (this.props.query != nextProps.query) {
+            this.doQuery();
+        }
+    },
+
+    getInitialState: function() {
+        return {
+            results: [],
+        };
+    },
+
+    setResults: function(results) {
+        this.state.results = results;
+    },
+
+    doQuery: function() {
+        hound(this.props.query, this.setResults.bind(this));
+    },
+
     render: function() {
-        return (
-            <ul>
-                {this.props.results.map(function(result) {
-                    return <li>{result}</li>
-                })}
-            </ul>
-        );
+        var renderedResults = this.state.results.map(function(result) {
+            return <li>{result}</li>
+        })
+
+        return <ul>{renderedResults}</ul>;
     }
 });
 
 var datasetConfigs = {
-    suggestions: {component: Bla},
-    footer: {component: Bla},
+    states: {component: StatesDataset},
 };
 
 React.renderComponent(
-    <root.componentStore.Search datasetConfigs={datasetConfigs} />,
+    root.componentStore.Search({datasetConfigs: datasetConfigs}),
     document.getElementById("search-bar")
 );
-
-// myController.setDatasetResults("suggestions", ["hi", "bob"])
