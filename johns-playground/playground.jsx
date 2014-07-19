fd7ddf61f65a3b172d0f3fb28657477026b78ea6
John Sullivan <josullivan@mozilla.com>
2014-07-19 11:39:00 -0700

    One possibility...

--- johns-playground/playground.jsx
+++ johns-playground/playground.jsx
@@ -8,40 +8,25 @@ var SearchInputComponent = React.createClass({
 
 var SearchResultsComponent = React.createClass({
     render: function() {
-        return (
-            <div>
-                {this.props.resultSets.map(function(resultSet) {
-                    return <div>{resultSet.component({results: resultSet.results})}</div>;
-                })}
-            </div>
-        );
+        var children = [];
+        for (var i = 0; i < this.props.ordering.length; ++i) {
+            var curName = this.props.ordering[i];
+            var curDataset = this.props.datasetConfigs[curName];
+            if (!curDataset) {
+                throw Error("Unknown dataset " + curName + " in ordering.");
+            }
+            children.push(
+                curDataset.component({
+                    key: curName,
+                    dataset: curDataset,
+                    results: this.props.datasetResults[curName] || [],
+                })
+            );
+        }
+
+        return <div>{children}</div>;
     }
 });
-
-var SearchComponent = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <SearchInputComponent />
-                <SearchResultsComponent
-                    dataSets={this.props.dataSets}
-                    resultSets={this.props.resultSets} />
-            </div>
-        );
-    }
-});
-
-var Controller = function($container, dataSets) {
-    if ($container.length != 1) {
-        throw Error("$container must contain a single element.");
-    }
-
-    this.component = React.renderComponent(
-        <SearchComponent dataSets={dataSets} resultSets={[]} />,
-        $container[0]
-    );
-}
-
 
 var Bla = React.createClass({
     render: function() {
@@ -55,10 +40,63 @@ var Bla = React.createClass({
     }
 });
 
-var dataSets = [
+var SearchComponent = React.createClass({
+    getInitialState: function() {
+        return {
+            // object< name:str => configuration:object< prop:str => value:any > >
+            // Configuration for each data set.
+            datasetConfigs: {},
+
+            // object< name:str => results:list<any> >
+            // The actual results we are or will be displaying.
+            datasetResults: {},
+
+            // list< name:str >
+            // Defines which result sets, and in which order, are displayed.
+            ordering: [],
+        };
+    },
+
+    handleChange: function(event) {
+        this.setState({value: event.target.value});
+    },
+
+    render: function() {
+        return (
+            <div>
+                <SearchInputComponent />
+                <SearchResultsComponent
+                    datasetConfigs={this.state.datasetConfigs}
+                    datasetResults={this.state.datasetResults}
+                    ordering={this.state.ordering} />
+            </div>
+        );
+    }
+});
+
+var Controller = function($container, datasetConfigs) {
+    var self = this;
+
+    if ($container.length != 1) {
+        throw Error("$container must contain a single element.");
+    }
+
+    self.render = function() {
+        React.renderComponent(
+            <SearchComponent />,
+            $container[0]
+        );
+    };
+
+    self.addDatasetConfig = function(name, config) {
+        self.datasetConfigs[name] = config;
+    }
+}
+
+var datasetConfigs = [
     {component: Bla, name: "suggestions"},
     {component: Bla, name: "dynamic"},
 ]
 
-var myController = new Controller($("#search-bar"), dataSets);
-myController.setSets([{component: Bla, results: ["HI", "BOB"]}]);
+var myController = new Controller($("#search-bar"), datasetConfigs);
+myController.render();
