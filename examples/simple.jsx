a29f5069d00efdd97b1d99b93c284d819231a21c
John Sullivan <josullivan@mozilla.com>
2014-07-20 10:29:35 -0700

    Made example directory.
    Run simple server in project root to test.

---
+++ examples/simple.jsx
@@ -0,0 +1,58 @@
+/** @jsx React.DOM */
+
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+    'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+    'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+    'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+    'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
+
+// constructs the suggestion engine
+var stateHound = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    // `states` is an array of state names defined in "The Basics"
+    local: $.map(states, function(state) { return { value: state }; })
+});
+ 
+// kicks off the loading/processing of `local` and `prefetch`
+stateHound.initialize();
+
+var StatesDataset = React.createClass({
+        mixins: [
+                searchbar.mixins.BaseDataset,
+                searchbar.mixins.BloodhoundDataset(stateHound.ttAdapter(), true)],
+
+        render: function() {
+                var renderedResults = [];
+                for (var i = 0; i < this.state.results.length; ++i) {
+                        var isSelected = this.props.highlightedIndex === i;
+                        var className = isSelected ? "selected" : "";
+
+                        console.log(this.state.results[i]);
+
+                        renderedResults.push(
+                            <a href="#" onMouseOver={this.getHoverHandler(i)}
+                                    className={className}>
+                                {this.state.results[i]}</a>
+                        );
+                }
+
+                this.numItems = this.state.results.length;
+
+                return <ul>{renderedResults}</ul>;
+        }
+});
+
+var datasetConfigs = {
+        states: {component: StatesDataset},
+};
+
+React.renderComponent(
+        searchbar.componentStore.Search({datasetConfigs: datasetConfigs}),
+        document.getElementById("searchbar-the-basics")
+);
